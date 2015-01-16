401f72762817bd1402b7e7b9bdaa71de7542266a
Roskataa <roskata.kolevw@gmail.com>
2015-01-16 20:32:21 +0200

    Without Zip

---
+++ Primitive Data Types and Variables/03. Variable in Hexadecimal Format/VariablesInHexadecimalFormat.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class VariablesInHexadecimalFormat
+{
+    static void Main()
+    {
+        int number = 0xFE;
+        Console.WriteLine(number);
+    }
+}
+
---
+++ Primitive Data Types and Variables/06. Strings and Objects/StringAndObjects.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class StringAndObjects
+{
+    static void Main()
+    {
+        string a = "Hello";
+        string b = "World";
+
+        object sometext = a + " " + b;
+        Console.WriteLine(sometext);
+
+        string c = (string)sometext;
+        Console.WriteLine(c);
+    }
+}
+
